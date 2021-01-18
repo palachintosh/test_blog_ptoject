17e1e6e85649481ea1b8cf5314696bbf75964742
palachintosh <parser9000@gmail.com>
2021-01-18 22:51:17 +0100

    add logging

--- mysite/bikes_monitoring/views.py
+++ mysite/bikes_monitoring/views.py
@@ -5,6 +5,9 @@ from django.http import HttpResponse
 from .PrestaRequest.mainp.PrestaRequest import PrestaRequest
 from .utils import CodeValidators
 
+import os.path
+import datetime
+
 try:
     from .PrestaRequest.mainp.api_secret_key import api_secret_key
 except:
@@ -13,8 +16,39 @@ except:
 
 # Create your views here.
 
+def logging(**kwargs):
+    param_dict = kwargs.get('kwargs')
+    
+    try:
+        with open('PRLog.txt', 'a') as f:
+            for i in param_dict.items():
+                print(i[0] + ': ' + i[1], file=f)
+    
+    except:
+        with open('PRLog.txt') as f:
+            for i in param_dict.items():
+                print(i.keys() + ': ' + i.values(), file=f)
+
+
+
 
 class BikeCheck(View):
+    def options(self, request):
+        def cors_headers_add(to_json=[]):
+            data = JsonResponse(
+                {to_json[0]: to_json[1]}
+            )
+
+            data["Access-Control-Allow-Origin"] = "http://my.kross.pl"
+            data["Access-Control-Allow-Methods"] = "GET, OPTIONS"
+            data["Access-Control-Allow-Credentials"] = "true"
+            data["Vary"] = "Origin"
+            data["Access-Control-Allow-Headers"] = "Origin, Access-Control-Allow-Origin, Accept, X-Requested-With, Content-Type"
+
+            return data
+        
+        return cors_headers_add(to_json=['TEST', 'TEST'])
+
     def get(self, request):
 
         def cors_headers_add(to_json=[]):
@@ -22,12 +56,18 @@ class BikeCheck(View):
                 {to_json[0]: to_json[1]}
             )
 
-            data["Access-Control-Allow-Origin"] = "my.kross.pl/"
-            data["Access-Control-Allow-Methods"] = "GET, OPTIONS"
-            # response["Access-Control-Max-Age"] = "1000"
-            data["Access-Control-Allow-Headers"] = "X-Requested-With, Content-Type"
+            data["Access-Control-Allow-Origin"] = "http://my.kross.pl"
+            data["Vary"] = "Origin"
+            data["Access-Control-Allow-Credentials"] = "true"
+            data["Access-Control-Allow-Headers"] = "Origin, Access-Control-Allow-Origin, Accept, X-Requested-With, Content-Type"
 
             return data
+
+        with open('log_method.txt', 'w') as f:
+            print(request.method, file=f)
+
+        if request.method == 'OPTIONS':
+            return cors_headers_add(to_json=['TEST', 'TEST'])
 
         number_validator = CodeValidators()
         filter_code = number_validator.is_code_valid(request)
@@ -52,19 +92,33 @@ class BikeCheck(View):
                         print(del_from_warehouse)
 
                         if del_from_warehouse != None:
-                            presta_get.presta_put(request_url=del_from_warehouse)
-                           
+                            put_data = presta_get.presta_put(request_url=del_from_warehouse)
                             
-                            return cors_headers_add(to_json=['success', 'all data updated!'])
+                            kwargs_data = {
+                                'DATE': str(datetime.datetime.now()),
+                                'DELETE_BIKE': del_bike,
+                                'SA_PUT_STATUS': str(apply_changes),
+                                'PUT_STATUS': str(put_data),
+                                'REQUEST_WAREHOUSE_URL': del_from_warehouse,
+                            }
+
+                            logging(kwargs=kwargs_data)
+                            return cors_headers_add(to_json=['success', filter_code.get('rex_code')])
 
 
             except Exception as e:
+                kwargs_data = {
+                    'DATE': str(datetime.datetime.now()),
+                    'ERROR': str(e),
+                }
+                logging(kwargs=kwargs_data)
+
                 return cors_headers_add(to_json=['error', str(e)])
-            
+
         return cors_headers_add(to_json=['TypeError', 'Invalid code!'])
 
 
 
     def post(self, request):
         if request.POST:
-            return HttpResponse("Frame number is required!")
+            return HttpResponse("POST not allow!")
